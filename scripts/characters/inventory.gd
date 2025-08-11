81aeb9a8e9d9d5289be9599d2dc6845563d844bf
Nick Avtges <navtges3@gmail.com>
2025-08-11 19:42:24 -0400

    Basic implementation for stat block and inventory

--- scripts/characters/inventory.gd
+++ scripts/characters/inventory.gd
@@ -3,3 +3,21 @@ class_name Inventory
 
 @export var weapon: Weapon
 @export var potions: Array[ItemStack]
+
+func use_potion(potion: Potion) -> Effect:
+	var index = potions.find(potion)
+	if index != -1:
+		var item_stack = potions[index]
+		item_stack.count -= 1
+		if item_stack.count <= 0:
+			print("Removing empty potion slot for %s" % potion.name)
+			potions.remove_at(index)
+		return potion.effect
+	return null
+
+func add_potion(potion: Potion, amount: int = 1) -> void:
+	var index = potions.find(potion)
+	if index != -1:
+		potions[index].count += amount
+	else:
+		potions.append(ItemStack.new(potion, amount))
--- scripts/characters/stat_block.gd
+++ scripts/characters/stat_block.gd
@@ -9,7 +9,7 @@ class_name StatBlock
 @export var max_nrg := 0
 @export var current_nrg := 0
 
-@export var attack := 0		 # modifies physical attacks
-@export var magic := 0		 # modifies magical attacks
-@export var defense := 0		 # modifies physical defense
-@export var resistance := 0	 # Modifies magical defense
+@export var attack := 0		# modifies physical attacks
+@export var magic := 0		# modifies magical attacks
+@export var defense := 0	# modifies physical defense
+@export var resistance := 0	# Modifies magical defense
