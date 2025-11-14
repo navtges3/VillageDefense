263edc8289a6f7ac02afe56db3b3441985c10c3b
Nick Avtges <navtges3@gmail.com>
2025-11-14 14:48:47 -0500

    Updated item rarity

--- scripts/data/item.gd
+++ scripts/data/item.gd
@@ -1,7 +1,23 @@
 extends Resource
 class_name Item
 
+enum Rarity {
+	COMMON,
+	RARE,
+	LEGENDARY
+}
+
 @export var name: String
 @export var description: String
 @export var theme: Theme = preload("res://assets/button_themes/regular/gray_button.tres")
 @export var value: int
+
+func rarity_to_string(rarity: Rarity) -> String:
+	match rarity:
+		Rarity.COMMON:
+			return "Common"
+		Rarity.RARE:
+			return "Rare"
+		Rarity.LEGENDARY:
+			return "Legendary"
+	return "Unknown"
