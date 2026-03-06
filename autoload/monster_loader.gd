395ae901f8b716c51707958400e0be5b7c394017
Nick Avtges <navtges3@gmail.com>
2026-03-06 13:21:09 -0500

    Refactor resource paths for characters and items
    - Deleted experience and gold item resources as they are no longer needed.
    - Updated quest resources to reflect new paths for monster resources, moving them under the characters directory.
    - Adjusted references in quests to point to the correct monster resources.
    - Changed potion resource paths in the default village to point to the characters directory.
    - Modified battle_character scene to use the updated path for goblin sprite frames.
    - Updated battle_tester script to load heroes and monsters from the new characters directory.

--- autoload/monster_loader.gd
+++ autoload/monster_loader.gd
@@ -1,9 +1,9 @@
 extends Node
 
 var monster_db: Dictionary = {
-	"Goblin": preload("res://resources/monsters/goblin/goblin.tres"),
-	"Orc": preload("res://resources/monsters/orc.tres"),
-	"Ogre": preload("res://resources/monsters/ogre.tres"),
+	"Goblin": preload("res://resources/characters/monsters/goblin/goblin.tres"),
+	"Orc": preload("res://resources/characters/monsters/orc.tres"),
+	"Ogre": preload("res://resources/characters/monsters/ogre.tres"),
 }
 
 func get_monster(monster_name: String, level: int = 1) -> Monster:
--- scripts/test/battle_tester.gd
+++ scripts/test/battle_tester.gd
@@ -10,19 +10,19 @@ var monster_resources: Array[Monster] = []
 func _ready():
 	load_heroes()
 	load_monsters()
-	
+
 func load_heroes():
 	hero_resources = [
-		preload("res://resources/heroes/knight/knight.tres"),
-		preload("res://resources/heroes/princess/princess.tres"),
-		preload("res://resources/heroes/assassin/assassin.tres"),
+		preload("res://resources/characters/heroes/knight/knight.tres"),
+		preload("res://resources/characters/heroes/princess/princess.tres"),
+		preload("res://resources/characters/heroes/assassin/assassin.tres"),
 	]
 	for i in hero_resources.size():
 		hero_dropdown.add_item(hero_resources[i].get_class_name(), i)
 
 func load_monsters():
 	monster_resources = [
-		preload("res://resources/monsters/goblin/goblin.tres"),
+		preload("res://resources/characters/monsters/goblin/goblin.tres"),
 	]
 	for i in monster_resources.size():
 		monster_dropdown.add_item(monster_resources[i].name, i)
@@ -31,19 +31,19 @@ func load_monsters():
 func _on_start_battle_pressed() -> void:
 	GameState.hero = hero_resources[hero_dropdown.get_selected_id()].duplicate()
 	print("Hero selected: ", GameState.hero.name)
-	
+
 	var monster_obj = MonsterRequirement.new()
 	monster_obj.monster = monster_resources[monster_dropdown.get_selected_id()]
 	monster_obj.target_amount = monster_spin_box.value
 	var test_quest = Quest.new()
 	test_quest.title = "Test Quest"
 	test_quest.monster_objectives.append(monster_obj)
-	
+
 	var config := BattleConfig.new()
 	config.hero = GameState.hero
 	config.quest = test_quest
 	config.is_test_battle = true
-	
+
 	ScreenManager.go_to_screen(ScreenManager.ScreenName.BATTLE, config)
 
 
